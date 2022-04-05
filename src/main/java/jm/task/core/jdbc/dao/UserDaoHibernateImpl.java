a430790e4e09dad3ad8a773fb91ed88f80c42061
Traindriver-M <sergioparovozov@mail.ru>
2022-04-05 16:24:01 +0300

    Практическая задача 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,71 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sessionFactory = new Util().getSessionFactory();
+    private Session session = null;
+
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
-
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT," +
+                " name VARCHAR(50), " +
+                "lastName VARCHAR(100), age INT);").executeUpdate();
+        session.getTransaction().commit();
     }
 
     @Override
     public void dropUsersTable() {
-
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS users;").executeUpdate();
+        session.getTransaction().commit();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        User user = new User(name, lastName, age);
+        session.save(user);
+        session.getTransaction().commit();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        User userReadFromDb = session.get(User.class, id);
+        session.remove(userReadFromDb);
+        session.getTransaction().commit();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        List<User> usersList = session.createQuery("SELECT i FROM User i", User.class)
+                .getResultList();
+        session.getTransaction().commit();
+        return usersList;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.createSQLQuery("DELETE FROM users;").executeUpdate();
+        session.getTransaction().commit();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,13 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private final String HOST = "jdbc:mysql://localhost:3306/users_db";
     private final String USERNAME = "root";
     private final String PASSWORD ="19770098";
+    private final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final String SHOWSQL = "true";
+    private final String DIALECT = "org.hibernate.dialect.MySQL8Dialect";
+    private final String CONTEXT = "thread";
     private Connection connection;
 
     public Util()  {
@@ -16,6 +25,18 @@ public class Util {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public SessionFactory getSessionFactory(){
+        Properties properties = new Properties();
+        properties.put("hibernate.connection.driver_class", DRIVER);
+        properties.put("hibernate.connection.url", HOST);
+        properties.put("hibernate.connection.username",USERNAME);
+        properties.put("hibernate.connection.password", PASSWORD);
+        properties.put("hibernate.show_sql", SHOWSQL);
+        properties.put("hibernate.dialect", DIALECT);
+        properties.put("hibernate.current_session_context_class", CONTEXT);
+        return new Configuration().setProperties(properties).addAnnotatedClass(User.class).buildSessionFactory();
     }
 
     public Connection getConnection(){
